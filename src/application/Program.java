ffe81ea240165c358f539207b29fc8bb8ab6edf5
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2020-10-29 13:41:16 -0300

    Acrescentada mensagem de Boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
 //		double xA, xB, xC, yA, yB, yC;
 		Triangle x, y;
 		x = new Triangle();
